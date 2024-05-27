336caddf39b54a0ae0363355f53d8ecae20bc716
Vidhya Chidambaram <vchidamb@redhat.com>
2024-05-27 17:07:55 -0400

    add more dependencies, delete default main class

--- fundamentals/src/main/java/com/cvidyac/kafka/ProducerDemo.java
+++ fundamentals/src/main/java/com/cvidyac/kafka/ProducerDemo.java
@@ -1,7 +1,7 @@
 package com.cvidyac.kafka;
 
-public class Main {
+public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
